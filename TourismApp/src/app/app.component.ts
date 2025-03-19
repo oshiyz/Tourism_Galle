672f8e2c47d699e501ad2e8545390bd2170dd4b6
alokaar <subathiaswini2002@gmail.com>
2025-03-19 22:53:54 +0530

    Homepage1 = header + footer

--- TourismApp/src/app/app.component.ts
+++ TourismApp/src/app/app.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/auth/shared/header/header.component';
+import { FooterComponent } from './components/auth/shared/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterModule], // Import RouterModule for <router-outlet>
-  templateUrl: './app.component.html',
+  imports: [RouterModule, RouterOutlet, HeaderComponent, FooterComponent], // Import components properly
+  template: `
+    <app-header></app-header>
+    <router-outlet></router-outlet>
+    <app-footer></app-footer>
+  `,
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
