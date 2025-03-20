a4d8de8abb00905ed8f867f973cbc9ce541b030d
alokaar <subathiaswini2002@gmail.com>
2025-03-20 20:50:48 +0530

    Aplace1

--- TourismApp/src/app/app.routes.ts
+++ TourismApp/src/app/app.routes.ts
@@ -3,11 +3,25 @@ import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/auth/login/login.component';
 import { RegisterComponent } from './components/auth/register/register.component';
+import { PlacesComponent } from './components/auth/places/places.component';
 
 export const routes: Routes = [
+<<<<<<< Updated upstream
+=======
+<<<<<<< Updated upstream
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: '**', redirectTo: 'login' } // Default route
+=======
+  { path: 'auth/places', component: PlacesComponent },
+>>>>>>> Stashed changes
   { path: 'auth/home', component: HomeComponent },
   { path: 'auth/login', component: LoginComponent },
   { path: 'auth/register', component: RegisterComponent },
   { path: '', redirectTo: 'auth/login', pathMatch: 'full' }, // Default route
   { path: '**', redirectTo: 'auth/login' } // Wildcard route
+<<<<<<< Updated upstream
+=======
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
 ];
